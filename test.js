bab2eec2ad85ee2bc694e6c25cb8b9b18f975702
ImNovix <bamcat641@gmail.com>
2026-03-26 08:56:31 -0500

    make the package into a class, simpiy logic, and update docs for the class

--- test.js
+++ test.js
@@ -1,14 +1,30 @@
-import tools from "./src/index.js";
+import libertyTools from "./src/index.js";
+import "dotenv/config";
+
+const client = new libertyTools({
+    SERVER_KEY: process.env.PRIVATE_SERVER_KEY,
+});
+
+await client.getPrivateServerAPI("?Players=true");
 
 /*
 console.log(await tools.createAPIData([
     "Players",
+    "Members",
     {
         t: "Command",
         r: ":h hello hello"
+    },
+    {
+        t: "Command",
+        r: ":tp me"
+    },
+    {
+        t: "Command",
+        r: ":tp player me"
     }
 ]));
 */
 
-for (let i = 0; i !== 50; i++) console.log(await tools.getPrivateServerAPI());
+//for (let i = 0; i !== 50; i++) console.log(await tools.getPrivateServerAPI());
 //for (let i = 0; i !== 20; i++) await tools.sendPrivateServerCommand({command: ":logs"});
